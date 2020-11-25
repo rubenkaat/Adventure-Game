223bf50a1a2c998b102293650d467ce6c32aabf3
Ruben Kaat <99034608@mydavinci.nl>
2020-11-25 13:11:37 +0100

    html pagina gemaakt + gestyled

---
+++ adventure.js
@@ -0,0 +1,6 @@
+function level1(){
+	document.getElementById("Title_Level").innerHTML = "Bij de Auto";
+	document.getElementById("Level_Description").innerHTML = "Het is koud buiten en je hebt het gevoel dat je word bekeken. Je ziet verderop het huis. Het lijkt verlaten. Wil je naar het huis toe gaan?";
+
+}
+level1()
